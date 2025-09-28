9df6f343c61f729ce7041b4b3787179d26ccdb56
Ritesh Pathak <pathakritesh2004@gmail.com>
2025-09-28 13:49:58 +0530

    message

--- SubArray.java
+++ SubArray.java
@@ -1,14 +1,17 @@
 class SubArray{
     public static void main(String[] args) {
         int a[]={1,2,3,4,5};
+        int Total_SubArray=0;
         for(int i=0;i<a.length;i++){
             for(int j=i;j<a.length;j++){
                 for(int k=i;k<=j;k++){
                     System.out.print(a[k]+" ");
                 }
+                Total_SubArray++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Total_SubArray = "+Total_SubArray);
     }
 }
